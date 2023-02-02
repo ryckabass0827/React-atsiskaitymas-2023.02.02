bf227ff91a8b38b4fc06453abc54009f524fe927
ryckabass0827 <laiskasrichardui@gmail.com>
2023-02-02 09:28:25 +0200

    created routes in app.jsx created footer, navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Add from "./pages/Add";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <></>
+    <Router>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Switch>
+        <Route exact path="/register">
+          <Register setIsLoggedIn={setIsLoggedIn} />
+        </Route>
+        <Route exact path="/login">
+          <Login setIsLoggedIn={setIsLoggedIn} />
+        </Route>
+        <Route exact path="/">
+          {isLoggedIn ? <Home /> : <Redirect to="/login" />}
+        </Route>
+        <Route exact path="/add">
+          {isLoggedIn ? <Add /> : <Redirect to="/login" />}
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <Link to="/">Logo</Link>
+            </div>
+            <ul className="navbar-links">
+                <li>
+                    <Link to="/home">Home</Link>
+                </li>
+                <li>
+                    <Link to="/add">Add</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
